58c9fb1f32ac9ce7fac5fed0ddfeb73f9b8aa4b6
ps <dhjkjdfk/@.1>
2022-12-04 09:58:33 +0300

    висилица + уровни сложности

--- Классная работа/main.py
+++ Классная работа/main.py
@@ -1,4 +1,4 @@
-HANGMANPICS = ['''
+HANGMANPICS0 = ['''
 
     +---+
     |   |
@@ -56,7 +56,77 @@ HANGMANPICS = ['''
         |
  =========''']
 
-def HANGMANPICS1():
+HANGMANPICS1 = ['''
+
+    +---+
+    |   |
+    |
+        |
+        |
+        |
+ =========''', '''
+
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+ =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+ =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+ =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+   / \  |
+        |
+ =========''']
+
+HANGMANPICS2 = ['''
+
+    +---+
+    |   |
+    |
+        |
+        |
+        |
+ =========''', '''
+
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+ =========''', '''
+
+    +---+
+    |   |
+    O   |
+   /|\  |
+   / \  |
+        |
+ =========''']
+
+
+
+def HANGMANPICSdef():
     import random
     words = [['дуб','тополь','берёза'],['хомяк','крыса','мышка'],['гитара','фортепиано','скрипка']]
     help = [['У Лукоморья ... зелёный','... пух','символ России'],['щекастый зверёк','подлый человек','сыр'],['струнный щипковый','Громко и тихо','звук похож на скрип']]
@@ -66,6 +136,15 @@ def HANGMANPICS1():
     health = 7
     used_letters = ''
     win_word = []
+
+    print('Выберите уровень сложности:', '(0) - начальный','(1) - средний','(2) - ультрасложный',sep = '\n')
+    r = int(input())
+    if r == 0:
+        health = 7
+    elif r==1:
+        health = 5
+    elif r==2:
+        health = 3
 
     print('Выберите тему и введите цифру:', '(0) - деревья','(1) - грызуны','(2) - музыкальные инструменты',sep = '\n')
     p = input()
@@ -82,7 +161,7 @@ def HANGMANPICS1():
                     if p == q:
                         word = words[q][random.randrange(3)]
                         e = 1
-        except ValueError:
+        except ValueError: #человек вводит именно число
             print('введите нужное ЧИСЛО')
             p = input()
 
@@ -119,29 +198,55 @@ def HANGMANPICS1():
                             win_word[r] = letter
                             test = True
 
-        if test == False:
-            if g == 0:
-                health -= 1
-                t = t+1
-            g = 0
-            print('Вы ввели неверную букву.')
-            print(HANGMANPICS[t-1])
-            print('Оставшееся кол-во попыток = ', health)
-        else:
-            print('Вы ввели верную буву')
-            print(win_word)
+        if r == 0:
+            if test == False:
+                if g == 0:
+                    health -= 1
+                    t = t+1
+                g = 0
+                print('Вы ввели неверную букву.')
+                print(HANGMANPICS0[t-1])
+                print('Оставшееся кол-во попыток = ', health)
+            else:
+                print('Вы ввели верную буву')
+                print(win_word)
+        elif r == 1:
+            if test == False:
+                if g == 0:
+                    health -= 1
+                    t = t+1
+                g = 0
+                print('Вы ввели неверную букву.')
+                print(HANGMANPICS1[t-1])
+                print('Оставшееся кол-во попыток = ', health)
+            else:
+                print('Вы ввели верную буву')
+                print(win_word)
+        elif r == 2:
+            if test == False:
+                if g == 0:
+                    health -= 1
+                    t = t+1
+                g = 0
+                print('Вы ввели неверную букву.')
+                print(HANGMANPICS2[t-1])
+                print('Оставшееся кол-во попыток = ', health)
+            else:
+                print('Вы ввели верную буву')
+                print(win_word)
+
         if word == (''.join(win_word)):
             print('Вы победили')
             print('Хотите сыграть ещё раз?','да/нет',sep = '\n')
             b = input()
             if b == 'да':
-                return HANGMANPICS1()
+                return HANGMANPICSdef()
             break
         elif health == 0:
             print('Вы проиграли')
             print('Хотите сыграть ещё раз?', 'да/нет', sep='\n')
             b = input()
             if b == 'да':
-                return HANGMANPICS1()
+                return HANGMANPICSdef()
             break
-HANGMANPICS1()
+HANGMANPICSdef()
